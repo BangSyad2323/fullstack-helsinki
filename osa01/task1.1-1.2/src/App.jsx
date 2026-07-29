f236907f64ab6d306d3912530db78d755fe47c66
17:45:39 <nurildani81@gmail.com>
2026-07-29 09:18:44 +0700

    completed task 1.3

--- osa01/task1.1-1.2/src/App.jsx
+++ osa01/task1.1-1.2/src/App.jsx
@@ -29,19 +29,25 @@ const Total = (props) => {
 }
 
 const App = () => {
-  const course = 'Half stack application development'
-  const part1 = 'Fundamenteal of React'
-  const exercise1 = 10
-  const part2 = 'Using props to pas data'
-  const exercise2 = 7
-  const part3 = 'State of a component'
-  const exercise3 = 14
+  const course = 'Half Stack application development'
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <div>
       <Header course={course}/>
-      <Content part1={part1} part2={part2} part3={part3} exercise1={exercise1} exercise2={exercise2} exercise3={exercise3} />
-      <Total exercise1={exercise1} exercise2={exercise2} exercise3={exercise3} />
+      <Content part1={part1.name} part2={part2.name} part3={part3.name} exercise1={part1.exercises} exercise2={part2.exercises} exercise3={part3.exercises} />
+      <Total exercise1={part1.exercises} exercise2={part2.exercises} exercise3={part3.exercises} />
     </div>
   )
 }
