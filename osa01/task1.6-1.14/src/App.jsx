5636ab4d9d95b209f99485e46e8fb77bb9cf78c4
17:45:39 <nurildani81@gmail.com>
2026-08-12 19:58:22 +0700

    task 1.8

--- osa01/task1.6-1.14/src/App.jsx
+++ osa01/task1.6-1.14/src/App.jsx
@@ -1,5 +1,19 @@
 import { useState } from 'react'
 
+const Statistic = (props) => {
+  return(
+    <div>
+      <h1>Statistic</h1>
+       <p>good: {props.good}</p>
+      <p>neutral: {props.neutral}</p>
+      <p>bad: {props.bad}</p>
+      <p>all: {props.all}</p>
+      <p>average: {props.average}</p>
+      <p>positive: {props.positive}% </p>
+    </div>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -20,14 +34,7 @@ const App = () => {
       <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
       <button onClick={() => setBad(bad + 1)}>bad</button>
 
-      <h1>statistik</h1>
-
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {all}</p>
-      <p>average: {average}</p>
-      <p>positive: {positive}% </p>
+      <Statistic good={good} neutral={neutral} bad={bad} all={all} average={average} positive={positive} />
     </div>
   )
 }
