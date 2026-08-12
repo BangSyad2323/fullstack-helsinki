1fcf508ac9c8c2a75a5362dc07f8ac7e45348c07
17:45:39 <nurildani81@gmail.com>
2026-08-12 20:29:44 +0700

    task 1.10

--- osa01/task1.6-1.14/src/App.jsx
+++ osa01/task1.6-1.14/src/App.jsx
@@ -1,5 +1,21 @@
 import { useState } from 'react'
 
+const Button = (props) => {
+  return(
+    <div>
+      <button onClick={props.onClick}>{props.text}</button>
+    </div>
+  )
+}
+
+const StatisticLine = (props) => {
+  return (
+    <div>
+      <p>{props.text} : {props.value}</p>
+    </div>
+  )
+}
+
 const Statistic = (props) => {
 
   if (props.all === 0) {
@@ -10,15 +26,16 @@ const Statistic = (props) => {
   return(
     <div>
       <h1>Statistic</h1>
-      <p>good: {props.good}</p>
-      <p>neutral: {props.neutral}</p>
-      <p>bad: {props.bad}</p>
-      <p>all: {props.all}</p>
-      <p>average: {props.average}</p>
-      <p>positive: {props.positive}% </p>
+      <StatisticLine text="good" value={props.good} />
+      <StatisticLine text="neutral" value={props.neutral} />
+      <StatisticLine text="bad" value={props.bad} />
+      <StatisticLine text="all" value={props.all} />
+      <StatisticLine text="average" value={props.average} />
+      <StatisticLine text="positif" value={props.positive} />
     </div>
   )
 }
+
 
 const App = () => {
   // save clicks of each button to its own state
@@ -32,13 +49,26 @@ const App = () => {
 
   const positive = (good / all) * 100
 
+  const addgood = () => {
+    setGood(good + 1);
+  }
+  const addneutral = () => {
+    setNeutral(neutral + 1);
+  }
+  const addbad = () => {
+    setBad(bad + 1);
+  }
+
+
   return (
     <div>
       <h1>give feedback</h1>
 
-      <button onClick={() => setGood(good + 1)}>good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-      <button onClick={() => setBad(bad + 1)}>bad</button>
+    
+
+      <Button  onClick={addgood} text="good"/>
+      <Button  onClick={addneutral} text="neutral"/>
+      <Button  onClick={addbad} text="bad"/>
 
       <Statistic good={good} neutral={neutral} bad={bad} all={all} average={average} positive={positive} />
     </div>
