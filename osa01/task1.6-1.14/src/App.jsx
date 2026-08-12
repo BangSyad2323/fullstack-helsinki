6e66235dfa19653c6a5a5dec65e8525f388dbed8
17:45:39 <nurildani81@gmail.com>
2026-08-12 19:28:52 +0700

    task 1.7

--- osa01/task1.6-1.14/src/App.jsx
+++ osa01/task1.6-1.14/src/App.jsx
@@ -6,6 +6,12 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const all = good + neutral + bad
+
+  const average = all / 3
+
+  const positive = (good / all) * 100
+
   return (
     <div>
       <h1>give feedback</h1>
@@ -19,6 +25,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average: {average}</p>
+      <p>positive: {positive}% </p>
     </div>
   )
 }
